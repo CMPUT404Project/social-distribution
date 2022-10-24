082bb152d5eb7b53a22453ab0ed6ec9fec5a81ee
Kevin Li <43177172+kli885@users.noreply.github.com>
2022-10-24 00:43:10 -0600

    Frontend Login Feature

--- backend/urls.py
+++ backend/urls.py
@@ -38,10 +38,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('api/', include('users.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('swagger/', schema_view.with_ui('swagger'), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc'), name='schema-redoc'),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/swagger/', schema_view.with_ui('swagger'), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc'), name='schema-redoc'),
 ]
