94885eab228089436c31825ce44c3c5dde4fcb08
Jonathan Martins <jcmartin@ualberta.ca>
2022-11-15 17:01:27 -0700

    Update default behavior to not use any pagination

--- backend/pagination.py
+++ backend/pagination.py
@@ -1,28 +1,27 @@
 from django.core.paginator import Paginator 
 
-#Custom pagination class, initialize with request to customize size and page
+#Custom pagination class, initialize with request
+#By default, if no page_size or page is provided to the query parameter, all objects are returned
 class CustomPagination():
     def __init__(self, context):
         self.context = context
-        self.page_size = 5
-        self.page = 1
+        self.query_params = self.context['request'].query_params
         self.setup()
 
     def setup(self):
+        # initialize and setup page size and page
         try:
-            self.page_size = self.context['request'].query_params.get('size')
-            self.page = self.context['request'].query_params.get('page')
+            page_size = self.query_params.get('size')
+            page = self.query_params.get('page')
+            self.page_size = page_size
+            self.page = page
         except:
             return
 
+
     # this function tkaes an object and orders it by order or default 'id'
     def paginate(self, obj, order='id'):
-        pagination = Paginator(obj.order_by(order), self.page_size)
-        return pagination.page(self.page)
-        
-
-        
-
-
-    
-
+        if self.page_size is None or self.page is None:
+            return obj.order_by(order)
+        self.pagination = Paginator(obj.order_by(order), self.page_size)
+        return self.pagination.page(self.page)    
