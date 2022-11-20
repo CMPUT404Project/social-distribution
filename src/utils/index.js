ce2f98d5389179cd6ef4e0ae79de69de8af791f8
Kevin Li <43177172+kli885@users.noreply.github.com>
2022-11-20 16:09:15 -0700

    Profile page
    Co-authored-by: byrontung <btung1@ualberta.ca>

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,41 @@
 import axios from 'axios';
- 
-export const setAxiosAuthToken = token => {
-   if (token) {
-       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-   }
-   else
-       delete axios.defaults.headers.common["Authorization"];
+import { Slide, SlideProps} from '@mui/material';
+
+export const regexPatterns = {
+    namePattern: /^[A-Za-z0-9]{1,30}$/,
+    gitPattern: /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i
 }
+
+export const namePattern = /^[A-Za-z0-9]{1,30}$/;
+export const gitPattern = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+
+export const setAxiosAuthToken = () => {
+    const token = localStorage.getItem('access_token') || sessionStorage.getItem('access_token');
+    if (token) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    } else {
+        delete axios.defaults.headers.common["Authorization"];
+    }
+}
+
+/**
+* Code from https://stackoverflow.com/a/68333175
+* By Caleb Taylor
+*/
+export const doesImageExist = (url) => new Promise((resolve) => {
+   const img = new Image();
+   img.src = url;
+   img.onload = () => resolve(true);
+   img.onerror = () => resolve(false);
+});
+
+export function SlideTransition(props: SlideProps) {
+    return <Slide{...props} direction="down"/>;
+}
+
+export const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
 export function clearStorage() {
     localStorage.clear();
